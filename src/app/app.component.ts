239dac21582ac85c70dfd5a7cff5dc7f8ce5fd28
Koman Rudden <koman.rudden@partner.bmw.co.za>
2024-10-08 11:04:31 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'multi-auth-ui';
+  title = 'multiple-auth-ui';
+
+  private matIconRegistry = inject(MatIconRegistry);
+  private domSanitizer = inject(DomSanitizer);
+
+  constructor() {
+    this.initSvgIcons();
+  }
+
+  private initSvgIcons() {
+    this.matIconRegistry.addSvgIcon(
+      'google_logo',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/icons/google_logo.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'github_logo',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/icons/github_logo.svg'
+      )
+    );
+  }
 }
